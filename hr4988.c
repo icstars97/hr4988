8bcef41981062885d0eb00c9382e16a2560b32a9
andrei <and15rei@gmail.com>
2020-01-31 00:12:28 +0300

    driver initialize and reset functions

---
+++ hr4988.c
@@ -0,0 +1,79 @@
+#include "hr4988.h"
+
+HR4988_DriverTypeDef ActiveDriversArr[MAX_DRIVERS_NUMBER];
+
+uint8_t active_drivers;
+
+void HR4988_DriverReset(HR4988_DriverTypeDef * DriverStruct){
+	
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->nEN,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->nRST,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->nSLEEP,GPIO_PIN_RESET);
+	
+	HAL_Delay(10);
+	
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->nRST,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->nSLEEP,GPIO_PIN_SET);
+	
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->DIR,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->MS1,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->MS2,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->MS3,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->STEP,GPIO_PIN_RESET);
+}
+
+void HR4988_SetStepMode(HR4988_DriverTypeDef * DriverStruct,HR4988_StepMode Mode){
+	
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->MS1,(GPIO_PinState)(Mode & (1<<2)));
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->MS2,(GPIO_PinState)(Mode & (1<<1)));
+	HAL_GPIO_WritePin(DriverStruct->GPIO_Port,DriverStruct->MS3,(GPIO_PinState)(Mode & (1<<0)));
+	
+}
+
+void HR4988_SetStepFreq(TIM_HandleTypeDef HalTimerHandle){
+	
+}
+
+void HR4988_RunMotor(HR4988_DriverTypeDef * DriverStruct,HR4988_Direction Dir){
+	
+}
+
+uint8_t HR4988_DriversInit(HR4988_DriverTypeDef * Drivers,uint8_t DriverNumber){
+	
+	uint8_t err_flag;
+	if (DriverNumber>MAX_DRIVERS_NUMBER){
+		
+		err_flag=0xFFU;
+	
+	}
+	else{
+		active_drivers=DriverNumber;
+		
+		for (uint8_t i=0;i<DriverNumber;i++){
+		
+			HR4988_DriverReset(&Drivers[i]);
+			ActiveDriversArr[i]=Drivers[i];
+			
+		}
+		
+		err_flag=0x00U;
+	
+	}
+	
+	return err_flag;		
+		
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	
+	for (uint8_t i=0;i<active_drivers;i++){
+		
+		if(htim->Instance==ActiveDriversArr[i].StartFreqTimer.Instance){
+			HAL_GPIO_TogglePin(ActiveDriversArr[i].GPIO_Port,ActiveDriversArr[i].STEP);
+			
+		}
+	}
+
+  
+}
---
+++ hr4988.h
@@ -0,0 +1,34 @@
+#include "main.h"
+
+#ifndef MAX_DRIVERS_NUMBER
+#define MAX_DRIVERS_NUMBER 1
+#endif
+
+typedef struct{
+	GPIO_TypeDef * GPIO_Port;
+	uint16_t nEN, MS1, MS2, MS3, nRST, nSLEEP, STEP, DIR;
+	TIM_HandleTypeDef StartFreqTimer, NormalFreqTimer;
+}HR4988_DriverTypeDef;
+
+typedef enum step_enum{
+	STEP_FULL=0x00U,
+	STEP_1_2=0x04U,
+	STEP_1_4=0x02U,
+	STEP_1_8=0x06U,
+	STEP_1_16=0x07U,
+	STEP_1_32=0x05U,
+	STEP_1_64=0x03U,
+	STEP_1_128=0x01U	
+}HR4988_StepMode;
+
+typedef enum dir_enum{
+	CW_DIR=0x00U,
+	CCW_dir
+	
+}HR4988_Direction;
+
+void HR4988_DriverReset(HR4988_DriverTypeDef * DriverStruct);
+
+void HR4988_SetStepMode(HR4988_DriverTypeDef * DriverStruct,HR4988_StepMode Mode);
+
+uint8_t HR4988_DriversInit(HR4988_DriverTypeDef * Drivers,uint8_t DriverNumber);
